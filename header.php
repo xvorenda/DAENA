197bf42647d3f138557ba42568b409441699b9d4
X. Valentine Orenda <xvorenda@vcu.edu>
2014-04-29 17:55:25 -0400

    kill futile attempt at IE support

--- header.php
+++ header.php
@@ -8,12 +8,5 @@
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='css/bootstrap.min.css' rel='stylesheet'>
 <link href='css/bootstrap.daena.css' rel='stylesheet'>
-
-
-    <!--[if lt IE 9]>
-      <script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>
-      <script src='https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script>
-    <![endif]-->
-
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script src='js/bootstrap.min.js'></script>
