360dd1eb9aea3ca462a0bb7c41f21aec8a60952e
X. Valentine Orenda <xvorenda@vcu.edu>
2014-04-29 00:50:57 -0400

    Fixing JS

--- header.php
+++ header.php
@@ -17,6 +17,6 @@ echo "
     <![endif]-->
 
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-<script src='js/bootstrap.min.js'></script>
+<script src='../js/bootstrap.min.js'></script>
 ";
 ?>
