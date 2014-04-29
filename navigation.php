3038567c97cc3c8999e3c64d7b3c2d6db719ee52
X. Valentine Orenda <xvorenda@vcu.edu>
2014-04-29 17:43:19 -0400

    maybe?

--- navigation.php
+++ navigation.php
@@ -1,6 +1,4 @@
-<?php
-echo "
-    <div class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
+      <div class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
       <div class='container-fluid'>
         <div class='navbar-header'>
           <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
@@ -65,5 +63,4 @@ echo "
            </div><!--/.nav-collapse -->
          </div>
        </div>
-    </div>";
-?>
+    </div>
