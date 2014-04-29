085f585c897b097b48e6f4354653f587573979ed
X. Valentine Orenda <xvorenda@vcu.edu>
2014-04-29 18:02:53 -0400

    header script didn't work, undo

--- header.php
+++ header.php
@@ -8,6 +8,5 @@
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='css/bootstrap.min.css' rel='stylesheet'>
 <link href='css/bootstrap.daena.css' rel='stylesheet'>
-<script type='text/javascript' src='http://www.pubapps.vcu.edu/identity/widgets/branding/black/responsive/js/vcubranding-black-1.0.js'></script>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script src='js/bootstrap.min.js'></script>
