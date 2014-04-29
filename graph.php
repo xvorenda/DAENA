980d6fea602a9c44e94ee59fb6db9cdcefa20954
X. Valentine Orenda <xvorenda@vcu.edu>
2014-04-29 17:47:40 -0400

    cleanup

--- graph.php
+++ graph.php
@@ -1,7 +1,3 @@
-<?php
-echo "    
-    <script src='../../js/highcharts.js'></script>
-    <script src='../../js/modules/exporting.js'></script>
+    <script src='js/highcharts.js'></script>
+    <script src='js/modules/exporting.js'></script>
     <div id='container' style='margin:96px 24px'></div>
-";
-?>
