f6db311aa76c2edd921b8f0890c4f9a0d84f7dca
X. Valentine Orenda <xvorenda@vcu.edu>
2014-04-29 00:29:52 -0400

    Forgot one

--- header.php
+++ header.php
@@ -3,12 +3,13 @@ echo "
 <html>
 <head>
 <title>DAENA | Data Aggregation and Emergency Notifications for Appliances</title>
-<link rel='stylesheet' href='css/daena.css'/>
 <link rel='shortcut icon' href='daena.png'/>
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='css/bootstrap.min.css' rel='stylesheet'>
+<link href='css/bootstrap.daena.css' rel='stylesheet'>
+
 
     <!--[if lt IE 9]>
       <script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>
