299417bccd51bece621736bb2f5c018faa288935
DAENA Admin <admin@daena.csbc.vcu.edu>
2014-09-10 11:28:47 -0400

    Integrating PHP-Login with Admin Panel

--- admin/index.php
+++ admin/index.php
@@ -1,145 +1,44 @@
 <?php
-/* Get things started */
-include "admin-header.php";
-include "../urlvars.php";
-echo "
-</head>
-<body>";
-$baseurl = '../index.php';
-include 'admin-nav.php';
-/* Start talking to MySQL and kill yourself if it ignores you */
-$daenaDB = new mysqli("localhost", "daena_user", "idontcareaboutpasswordsrightnow", "daena_db");
-if ($daenaDB === FALSE) {
-    die(mysql_error()); // TODO: better error handling
+
+/**
+ * A simple, clean and secure PHP Login Script / MINIMAL VERSION
+ * For more versions (one-file, advanced, framework-like) visit http://www.php-login.net
+ *
+ * Uses PHP SESSIONS, modern password-hashing and salting and gives the basic functions a proper login system needs.
+ *
+ * @author Panique
+ * @link https://github.com/panique/php-login-minimal/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// checking for minimum PHP version
+if (version_compare(PHP_VERSION, '5.3.3', '<')) {
+    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+} else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+    // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+    require_once("libraries/password_compatibility_library.php");
 }
 
-/* Ask MySQL about which probes exist and get their metadata */
-$allprobesquery = "SELECT SQL_CALC_FOUND_ROWS *
-FROM daena_db.probes 
-ORDER BY ABS(probe_id)";
-$allprobes = $daenaDB->query($allprobesquery);
-if($allprobes === FALSE) {
-    die(mysql_error()); // TODO: better error handling
-    
-/* Count the active probes for density handling */
-$countquery = "SELECT FOUND_ROWS()";
-	$countraw = mysql_query($countquery);
-	$countarray = mysql_fetch_assoc($countraw);
-	$count = implode(",",$countarray);
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// load the login class
+require_once("classes/Login.php");
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are logged in" view.
+    include("views/logged_in.php");
+    include("mod-all.php");
+
+} else {
+    // the user is not logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("views/not_logged_in.php");
 }
-/* Ask MySQL about which freeers exist and get their metadata */
-$allfreezersquery = "SELECT SQL_CALC_FOUND_ROWS *
-FROM daena_db.freezers 
-ORDER BY ABS(freezer_id)";
-$allfreezers = $daenaDB->query($allfreezersquery);
-if($allfreezers === FALSE) {
-    die(mysql_error()); // TODO: better error handling
-}
-/* Count the active freezers for density handling */
-$countquery = "SELECT FOUND_ROWS()";
-	$countraw = $daenaDB->query($countquery);
-	$countarray = $countraw->fetch_assoc();
-	$count = implode(",",$countarray);
-$i = 0;
-
-/* Draw Probe Mod Area */
-echo "
-<div class='probesbox'>
-<table>
-<tr><td>Probe ID</td><td>Probe Type</td><td>Probe Range</td><td>Active</td><td>Probe Hostport</td><td>Probe NTMS Port</td><td>&nbsp;</td></tr>
-";
-while(($probedata = $allprobes->fetch_assoc())){
-    $probe_id = $probedata['probe_id'];
-    $probe_type = $probedata['probe_type'];
-    $probe_range = $probedata['probe_range'];
-    $probe_active = $probedata['probe_active'];
-    $probe_ntms_port = $probedata['probe_ntms_port'];
-    $probe_hostport = $probedata['probe_hostport'];
-
-echo "<tr>
-        <form action='probe-mod.php' method='POST'>
-        <td><input type='text' class='input-medium search-query field-narrow' name='probe_id' value='".$probe_id."'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_type' value='".$probe_type."'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_range' value='".$probe_range."'/></td>
-        <td class='field-narrow'><input type='text' class='input-medium search-query field-narrow' name='probe_active' value='".$probe_active."'/></td>
-        <td class='field-wide'><input type='text' class='input-medium search-query field-wide' name='probe_hostport' value='".$probe_hostport."'/></td>
-        <td class='field-narrow'><input type='text' class='input-medium search-query field-narrow' name='probe_ntms_port' value='".$probe_ntms_port."'/></td>
-        <td><input type='text' class='stealth' name='mysqlaction' value='modify'/><input type='submit' name='submit' class='btn' value='Modify'/></td></form>
-</tr>";}
-
-echo "<tr>
-        <form action='probe-mod.php' method='POST'>
-        <td><input type='text' class='input-medium search-query field-narrow' name='probe_id'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_type'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_range'/></td>
-        <td class='field-narrow'><input type='text' class='input-medium search-query field-narrow' name='probe_active'/></td>
-        <td class='field-wide'><input type='text' class='input-medium search-query field-wide' name='probe_hostport'/></td>
-        <td class='field-narrow'><input type='text' class='input-medium search-query field-narrow' name='probe_ntms_port'/></td>
-        <td><input type='text' class='stealth' name='mysqlaction' value='add'/><input type='submit' name='submit' class='btn' value='Add'/></td></form>
-    </tr>
-</table>";
-
-
-
-
-/* Draw Freezer Mod Area */
-echo "
-<div class='freezersbox'>
-<table>
-<tr><td>Freezer Name</td><td>Building</td><td>Room Number</td><td>Temperature Range</td><td>NTMS Host</td><td>NTMS Port</td><td>Active</td><td>Graph Color</td><td>Freezer ID</td><td>&nbsp;</td></tr>
-";
-while(($freezerdata = $allfreezers->fetch_assoc())){
-    $freezer_name = $freezerdata['freezer_name'];
-    $freezer_location = $freezerdata['freezer_location'];
-    $freezer_temp_range = $freezerdata['freezer_temp_range'];
-    $freezer_id = $freezerdata['freezer_id'];
-    $freezer_active = $freezerdata['freezer_active'];
-    $freezer_color = $freezerdata['freezer_color'];
-    $location = explode("<br>", $freezer_location);
-        $freezer_location_building = $location[0];
-        $freezer_location_room = $location[1];
-    $probequery = "SELECT probe_hostport FROM daena_db.probes 
-    WHERE freezer_id='" . $freezer_id . "'";
-    $proberesult = $daenaDB->query($probequery);
-    while($probe = $proberesult->fetch_array()) {
-    $probe_hostport = $probe['probe_hostport'];
-    $hostport = explode(" ", $probe_hostport);
-        $probe_host = $hostport[0];
-        $probe_port = $hostport[1]; };
-
-echo "<tr>
-        <form action='freezer-mod.php' method='POST'>
-        <td><input type='text' class='input-medium search-query' name='freezer_name' value='".$freezer_name."'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_location_building' value='".$freezer_location_building."'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_location_room' value='".$freezer_location_room."'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_temp_range' value='".$freezer_temp_range."'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_host' value='".$probe_host."'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_port' value='".$probe_port."'/></td>
-        <td><input type='text' class='input-medium search-query field-narrow' name='freezer_active' value='".$freezer_active."'/></td>
-        <td><input type='text' class='input-medium search-query color' name='freezer_color' value='".$freezer_color."'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_id' value='".$freezer_id."'/></td>
-        <td><input type='text' class='stealth' name='mysqlaction' value='modify'/><input type='submit' name='submit' class='btn' value='Modify'/></td></form>
-    </tr>";
-		$i++;};
-
-echo "<tr>
-        <form action='freezer-mod.php' method='POST'>
-        <td><input type='text' class='input-medium search-query' name='freezer_name' value='New Freezer'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_location_building'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_location_room'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_temp_range'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_host'/></td>
-        <td><input type='text' class='input-medium search-query' name='probe_port'/></td>
-        <td><input type='text' class='input-medium search-query field-narrow' name='probe_active'/></td>
-        <td><input type='text' class='input-medium search-query color' name='freezer_color'/></td>
-        <td><input type='text' class='input-medium search-query' name='freezer_id'/></td>
-        <td><input type='text' class='stealth' name='mysqlaction' value='add'/><input type='submit' name='submit' class='btn' value='Add'/></form></td>
-    </tr>
-</table>
-</div></div>";	
-   
-
-/* Wrap things up */
-include '../footer.php';
-?>
-	    
