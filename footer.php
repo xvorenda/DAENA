9b534d1d42d73aab4a2e435759fd6d194437aa17
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-10 11:36:59 -0400

    Attempting to add page load time readout

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
         <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>, and 
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
         DAENA is free software released under GNU/GPLv3 and/or <a href='http://creativecommons.org/licenses/by/3.0/'>CC BY 3.0</a>.</p>
+        <p id = "loadingtime"></p>
     </div>  
   </body>
 </html>
