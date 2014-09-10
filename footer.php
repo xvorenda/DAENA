50db599defcfcde5bfd4103db61e8fcd0bbf160d
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-10 11:42:56 -0400

    Page Load Timer, Take 3

--- footer.php
+++ footer.php
@@ -1,19 +1,3 @@
- <script type="text/javascript">
-function pageload()
-{
-    var after = (new Date()).getTime();
-    var sec = (after-before)/1000;
-    var p = document.getElementById("loadingtime");
-    p.innerHTML = "Page load: " + sec + " seconds.";
-        
-}
-</script>
-<script type="text/javascript">
-    window.onload = function () 
-    { 
-        pageload();
-    }
-</script>
     <div class='navbar-fixed-bottom hidden-sm hidden-xs'>
         <p class='navbar-fixed-bottom-p'>Written for 
         <a href='http://www.vcu.edu/' target='_blank'>VCU</a> by 
@@ -24,7 +8,7 @@ function pageload()
         <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>, and 
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
         DAENA is free software released under GNU/GPLv3 and/or <a href='http://creativecommons.org/licenses/by/3.0/'>CC BY 3.0</a>.</p>
-        <p id = "loadingtime"></p>
+        <p id="loadingtime"></p>
     </div>  
  <script type="text/javascript">
 function pageload()
