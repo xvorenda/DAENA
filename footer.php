cbee8718b817a9b816626831744f0f4a12eb8d2b
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-10 11:56:27 -0400

    Page Load Time Formatting

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
     <div class='navbar-fixed-bottom hidden-sm hidden-xs'>
-        <p class='navbar-fixed-bottom-p' id="loadingtime"></p>
         <p class='navbar-fixed-bottom-p'>Written for 
         <a href='http://www.vcu.edu/' target='_blank'>VCU</a> by 
         <a href='http://www.people.vcu.edu/~xvorenda/' target='_blank'>XVO</a> and 
@@ -9,6 +8,7 @@
         <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>, and 
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
         DAENA is free software released under GNU/GPLv3 and/or <a href='http://creativecommons.org/licenses/by/3.0/'>CC BY 3.0</a>.</p>
+        <p class='navbar-fixed-bottom-p' id="loadingtime"></p>
     </div>  
 <script type="text/javascript">
     window.onload = function () 
