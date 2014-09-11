39ec3fe28615b5e55ed142a48a933beda0fab186
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-11 14:50:44 -0400

    Can I stay logged in this way?

--- admin/probe-view.php
+++ admin/probe-view.php
@@ -1,5 +1,13 @@
 <?php
 /* Get things started */
+require_once("libraries/password_compatibility_library.php");
+// include the configs / constants for the database connection
+require_once("config/db.php");
+// load the login class
+require_once("classes/Login.php");
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
 include "admin-header.php";
 $baseurl = '../index.php';
 include 'admin-nav.php';
