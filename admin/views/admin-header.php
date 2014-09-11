d8637cfa20737495912d3043134362104e55731b
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-11 15:03:22 -0400

    No...

--- admin/views/admin-header.php
+++ admin/views/admin-header.php
@@ -7,8 +7,8 @@ echo "
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
-<link href='../../css/bootstrap.css' rel='stylesheet'>
-<link href='../../css/bootstrap.daena.css' rel='stylesheet'>
+<link href='../css/bootstrap.css' rel='stylesheet'>
+<link href='../css/bootstrap.daena.css' rel='stylesheet'>
 
 
     <!--[if lt IE 9]>
