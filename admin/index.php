861acb441a5867a509ddf4fb204003a931e8519d
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-11 14:16:47 -0400

    More PHP-Login Integration Testing

--- admin/index.php
+++ admin/index.php
@@ -26,8 +26,6 @@ require_once("config/db.php");
 // load the login class
 require_once("classes/Login.php");
 
-include("admin-nav");
-
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process. in consequence, you can simply ...
 $login = new Login();
@@ -36,7 +34,6 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/logged_in.php");
     include("mod-all.php");
 
 } else {
