44abb2e7440c0efdbd9f1f137597e5dc420889c5
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-11 13:47:25 -0400

    Tweak

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ function pageload()
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
-<link href='css/bootstrap.min.css' rel='stylesheet'>
+<link href='css/bootstrap.css' rel='stylesheet'>
 <link href='css/bootstrap.daena.css' rel='stylesheet'>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script src='js/bootstrap.min.js'></script>
