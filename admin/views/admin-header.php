b128099aee07b2f5f316384f871b449bb99345d8
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-12 13:38:56 -0400

    Adding login to header instead of per page

--- admin/views/admin-header.php
+++ admin/views/admin-header.php
@@ -1,4 +1,12 @@
 <?php
+require_once("libraries/password_compatibility_library.php");
+// include the configs / constants for the database connection
+require_once("config/db.php");
+// load the login class
+require_once("classes/Login.php");
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
 echo "
 <html>
 <head>
