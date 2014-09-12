e8b1b7808252500cb969a0731efc650b7b3aeaf5
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-12 18:17:01 -0400

    Citation Added

--- assets/footer.php
+++ assets/footer.php
@@ -5,6 +5,7 @@
 	<a href='http://www.people.vcu.edu/~voegtlylj/' target='_blank'>LJV</a> using 
         <a href='http://www.python.org/' target='_blank'>Python</a>, 
         <a href='http://www.mysql.com/' target='_blank'>MySQL</a>, 
+        <a href='http://www.php-login.net/' target='_blank'>PHP-Login</a>, 
         <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>, and 
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
         DAENA is free software released under GNU/GPLv3 and/or <a href='http://creativecommons.org/licenses/by/3.0/'>CC BY 3.0</a>.</p>
