450d229ee604ba710b73a6234df297b4c5fc37b0
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-13 00:00:59 -0400

    Fix favicon in admin

--- admin/assets/admin-header.php
+++ admin/assets/admin-header.php
@@ -11,7 +11,7 @@ echo "
 <html>
 <head>
 <title>DAENA | Data Aggregation and Emergency Notifications for Appliances</title>
-<link rel='shortcut icon' href='../daena.png'/>
+<link rel='shortcut icon' href='../../daena.png'/>
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
