7353dc102bfe1c5556348bf06d5e0c908dec2f6c
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-12 21:56:32 -0400

    Adapting Groups off of Freezers

--- admin/handlers/group-mod.php
+++ admin/handlers/group-mod.php
@@ -1,8 +1,64 @@
 <?php
+/* Get things started */
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$mysqlaction = filter_input(INPUT_POST, 'mysqlaction');
+$group_id = filter_input(INPUT_POST, 'group_id');
+$group_name = filter_input(INPUT_POST, 'group_name');
+$group_desc = filter_input(INPUT_POST, 'group_desc');
 
+/* Start talking to MySQL and kill yourself if it ignores you */
+include 'admin/config/db.php';
+$daenaDB = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  /* Add a Group */
+$groupadd = "INSERT INTO daena_db.groups 
+    (group_id, group_name, group_desc)
+VALUES
+    ('".$group_id."', '".$group_name."', '".$group_desc."')";
+
+/* Mod a Freezer */
+$groupupdate = "UPDATE daena_db.groups
+SET group_name='" . $group_name . "', group_desc='" . $group_desc . "'
+WHERE group_id='" . $group_id . "'";
+
+if ($mysqlaction = "modify") {
+
+$onegroup = $daenaDB->query($groupupdate);
+if($onegroup === FALSE) {
+    die(mysqli_error()); // TODO: better error handling
+}
+echo "Modification Success!";
+echo '<script>window.location.replace("';
+$pageURL = 'http://';
+if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"];
+ }
+ echo $pageURL;
+ echo '/admin/groups.php");</script>';
+}
+
+if ($mysqlaction = "add") {
+
+$onegroup = $daenaDB->query($groupadd);
+if($onegroup === FALSE) {
+    die(mysqli_error()); // TODO: better error handling
+}
+echo 'Addition Success!';
+echo '<script>window.location.replace("';
+$pageURL = 'http://';
+if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"];
+ }
+ echo $pageURL;
+ echo '/admin/groups.php");</script>';
+}
+?>
