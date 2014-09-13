1f9ceafc868a6896591e8ad0941d489521894187
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-12 22:47:04 -0400

    Beef up passwords, change text

--- admin/assets/register.php
+++ admin/assets/register.php
@@ -23,14 +23,14 @@ if (isset($registration)) {
     <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
 
     <!-- the email input field uses a HTML5 email type check -->
-    <label for="login_input_email">User's email</label>
+    <label for="login_input_email">Email Address</label>
     <input id="login_input_email" class="login_input" type="email" name="user_email" required />
 
-    <label for="login_input_password_new">Password (min. 6 characters)</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+    <label for="login_input_password_new">Password (12+ characters)</label>
+    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{12,}" required autocomplete="off" />
 
     <label for="login_input_password_repeat">Repeat password</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{12,}" required autocomplete="off" />
     <br><br>
     <input type="submit"  name="register" class= "btn" value="Register" />
 
