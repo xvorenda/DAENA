084bea34f01bedb571edd57cb3967f5cf868693d
Logan Voegtly <loganvoegtly@me.com>
2014-09-15 15:24:35 -0400

    Starting the process of using Google Charts
    Using Google Charts default example to test.

--- index.array.php
+++ index.array.php
@@ -3,6 +3,8 @@
 /* Get things started */
 include "assets/header.php";
 
+ 
+include "/assets/gc.array.php";
 /* Teach PHP how to read URL parameters, plus add defaults */
 include "assets/urlvars.php";
 
@@ -254,8 +256,9 @@ echo"
 </head>
 
 <body>";
-
-include 'assets/navigation.php';
+<!--Div that will hold the pie chart-->
+    <div id="chart_div" style="width:400; height:300"></div>
+#include 'assets/navigation.php';
 
 /* Actually draw the graph */
 include "assets/graph.php";
