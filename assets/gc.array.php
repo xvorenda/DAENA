16e97e4f4f7750052c342fdbe01d1aa07cd28206
Logan Voegtly <loganvoegtly@me.com>
2014-09-18 09:31:32 -0400

    Data in google charts
    Attempt to put the data into google charts

--- assets/gc.array.php
+++ assets/gc.array.php
@@ -16,24 +16,17 @@ echo"
   function drawChart() {
 
   // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-	['Mushrooms', 3],
-	['Onions', 1],
-	['Olives', 1],
-	['Zucchini', 1],
-	['Pepperoni', 2]
-  ]);
+  var data = new google.visualization.DataTable(".$json_chart.");
+
+
 
   // Set chart options
-  var options = {'title':'How Much Pizza I Ate Last Night',
-				 'width':400,
-				 'height':300};
+//  var options = {'title':'How Much Pizza I Ate Last Night',
+//				 'width':400,
+//				 'height':300};
 
   // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
   chart.draw(data, options);
 }
 </script>
