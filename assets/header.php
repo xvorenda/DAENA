58d687857dc61bca724b452fc050b639c61b0711
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-18 01:39:37 -0400

    Tidying Up

--- assets/header.php
+++ assets/header.php
@@ -5,7 +5,7 @@
 <script type="text/javascript">
 before = (new Date()).getTime();
 </script>
- <script type="text/javascript">
+<script type="text/javascript">
 function pageload()
 {
     var after = (new Date()).getTime();
@@ -15,12 +15,14 @@ function pageload()
         
 }
 </script>
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
+<script src='js/bootstrap.min.js'></script>
 <title>DAENA | Data Aggregation and Emergency Notifications for Appliances</title>
-<link rel='shortcut icon' href='images/daena.png'/>
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='css/bootstrap.css' rel='stylesheet'>
 <link href='css/bootstrap.daena.css' rel='stylesheet'>
-<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-<script src='js/bootstrap.min.js'></script>
+<link rel='shortcut icon' href='images/daena.png'/>
+
+
