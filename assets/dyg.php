3768700e335caed2ad6ca517a75bd41fc3c6579a
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-17 23:59:55 -0400

    Dygraphs Implementation

---
+++ assets/dyg.php
@@ -0,0 +1,6 @@
+<?php
+echo "
+    <script type='text/javascript' src='js/dygraph-combined.js'></script>
+  </head>
+<body>";
+?>
---
+++ index.dyg.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Get things started */
+include 'assets/header.php';
+
+/* Teach PHP how to read URL parameters, plus add defaults */
+include 'assets/urlvars.php';
+
+/* Define the HighChart */
+include 'assets/dyg.php';
+        
+/* Set up navigation for different graphs || TODO: groups table, dynamically generate || */
+include "assets/url.php";
+$url = curPageURL();
+$baseurl = substr($url, 0, strpos($url, "?"));
+
+include 'assets/navigation.php';
+
+/* Actually draw the graph */
+include "assets/graph.dyg.php";
+
+/* Wrap things up */
+include "assets/footer.php";
+?>
