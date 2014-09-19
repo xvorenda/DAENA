1f35e88685e60f81455b8df270f49d5a1429a52f
Logan Voegtly <loganvoegtly@me.com>
2014-09-19 15:03:10 -0400

    Script...
    make it a java script and use div to display…

--- assets/dyg.array.php
+++ assets/dyg.array.php
@@ -1,9 +1,11 @@
 <?php
 echo"
-  new Dygraph(document.getElementById('graphdiv2'),
+<script  type='text/javascript'>
+  new Dygraph(document.getElementById('graph'),
 			".json_encode($json_chart)."
               );
-
+</script>
+<div id=graph></div>
 
 "
 ?>
