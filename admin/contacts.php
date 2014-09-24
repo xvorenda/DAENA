b14745bd45751c0b33c3480f888ef49fc1f338ce
Logan Voegtly <loganvoegtly@me.com>
2014-09-24 14:48:22 -0400

    add contact
    added contact and post to commit the insert into the db

--- admin/contacts.php
+++ admin/contacts.php
@@ -126,9 +126,83 @@ if ($login->isUserLoggedIn() == true)
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> <--close panel group-->
 		";
-	}
+	} // end while loop
+	echo"
+			<div class='panel-group' >
+				<div class='panel panel-default'>
+					<div class='panel-heading'>
+						<h2 class='panel-title'>
+							<a data-toggle='collapse'  href='#newcontact'>
+								Add Contact
+							</a>
+						</h2>
+					</div>
+					<div id='newcontact' class='panel-collapse collapse'>
+						<div class='panel-body table-responsive'>
+							<form action='handlers/contact-add-mod.php' method='POST'>
+								<table class='table'>
+									<tr><td>Name</td><td>Email</td><td>Alt Email</td></tr>
+									<tr>
+											<td><input type='text' class='input-large search-query' name='contact_name' value='".$contact_name."'/></td>
+											<td><input type='text' class='input-large search-query' name='contact_email' value='".$contact_email."'/></td>
+											<td><input type='text' class='input-large search-query' name='contact_alt_email' value='".$contact_alt_email."'/></td>
+										</form>
+									</tr>
+								</table>
+								<h3> Select freezers and alarm levels for the new person to get notifications. </h3>
+								<table class='table table-striped table-hover'>
+									<tr>
+										<td class='field-medium'>Freezer Name</td>
+										<td class='field-medium'>Freezer ID</td>
+										<td class='field-medium'>Normal State</td>
+										<td class='field-medium'>High Alarm 1</td>
+										<td class='field-medium'>High Alarm 2</td>
+										<td class='field-medium'>Critical Alarm</td>
+										<td class='field-medium'>Silenced Critical Alarm</td>
+										<td class='field-medium'>Critical to High Alarm</td>
+										<td class='field-medium'>Com Alarm</td>
+										<td class='field-medium'>Silenced Com Alarm</td>
+									</tr>";
+						
+					
+								$freezeralarmquery = "SELECT freezers.freezer_name, freezers.freezer_id 
+									FROM freezers 
+									WHERE freezers.freezer_active = 1 
+									ORDER BY freezers.freezer_id";
+								$allfreezerdata = $daenaDB->query($freezeralarmquery);
+								while(($freezerdata = $allfreezerdata->fetch_assoc()))
+								{
+									$freezer_name = $freezerdata['freezer_name'];
+									$freezer_id = $freezerdata['freezer_id'];
+
+									echo"
+									<tr class='alarm-table-row'>
+										<td>".$freezer_name."</td>
+										<td><input type='text' class='stealth' name='freezer_id[]' value='".$freezer_id."'/>".$freezer_id."</td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm0'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm1'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm2'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm3'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm4'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm5'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm6'  value='1'/></td>
+										<td><input type='checkbox'  name='".$freezer_id."alarm7'  value='1'/></td>
+									</tr>
+									";
+						
+								}
+								echo "
+								</table>
+								<input type='text' class='stealth' name='mysqlaction' value='add'/>
+								<input type='submit' name='add_contact' class='btn' value='add'/></td>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div> <--close panel group-->
+		";
 		/*
 		echo "<tr>
 				<form action='handlers/contact-mod.php' method='POST'>
---
+++ admin/handlers/contact-add-mod.php
@@ -0,0 +1,92 @@
+<?php
+/* Get things started */
+
+$mysqlaction = filter_input(INPUT_POST, 'mysqlaction');
+$contact_name = filter_input(INPUT_POST, 'contact_name');
+$contact_email = filter_input(INPUT_POST, 'contact_email');
+$contact_alt_email = filter_input(INPUT_POST, 'contact_alt_email');
+/* Start talking to MySQL and kill yourself if it ignores you */
+include('../config/db.php');
+$daenaDB = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if ($daenaDB->connect_errno) 
+{
+    printf("Connect failed: %s\n", $daenaDB->connect_error);
+    exit();
+}
+
+  /* Add a Contact */
+/*
+$groupadd = "INSERT INTO daena_db.groups 
+    (group_id, group_name, group_desc)
+	VALUES
+    ('".$group_id."', '".$group_name."', '".$group_desc."')";
+*/
+/* Mod a Contact */
+$contactadd = "INSERT INTO daena_db.contacts
+	SET name='".$contact_name."', 
+		email='".$contact_email."', 
+		alt_email=".$contact_alt_email."'";
+
+$error = 0;
+
+if ($mysqlaction == "add") 
+{
+	if (!$daenaDB->query($contactupdate)) 
+	{
+		printf("Errormessage: %s\n", $daenaDB->error);
+		$error=1;
+	}
+	$contact_id = $daenaDB->insert_id;
+
+	foreach( $_POST['freezer_id'] as $current_freezer_id) 
+	{
+		$alarm0 = (isset($_POST[$current_freezer_id.'alarm0'])) ? 1 : 0;
+		$alarm1 = (isset($_POST[$current_freezer_id.'alarm1'])) ? 1 : 0;
+		$alarm2 = (isset($_POST[$current_freezer_id.'alarm2'])) ? 1 : 0;
+		$alarm3 = (isset($_POST[$current_freezer_id.'alarm3'])) ? 1 : 0;
+		$alarm4 = (isset($_POST[$current_freezer_id.'alarm4'])) ? 1 : 0;
+		$alarm5 = (isset($_POST[$current_freezer_id.'alarm5'])) ? 1 : 0;
+		$alarm6 = (isset($_POST[$current_freezer_id.'alarm6'])) ? 1 : 0;
+		$alarm7 = (isset($_POST[$current_freezer_id.'alarm7'])) ? 1 : 0;
+	
+
+		/* Mod a Contact */
+		$alarmcontactinsert = "INSERT INTO daena_db.freezer_alarm_contacts
+			SET freezer_id='".$current_freezer_id."',
+				contact_id='".$contact_id."'.
+				alarm0='".$alarm0."', 
+				alarm1='".$alarm1."', 
+				alarm2='".$alarm2."', 
+				alarm3='".$alarm3."', 
+				alarm4='".$alarm4."', 
+				alarm5='".$alarm5."', 
+				alarm6='".$alarm6."', 
+				alarm7='".$alarm7."'";
+	
+		if (!$daenaDB->query($alarmcontactinsert)) 
+		{
+			printf("Errormessage: %s\n", $daenaDB->error);
+			$error = 1;
+		}
+	}
+	if($error==0)
+	{
+		echo "Modification Success!";
+		echo '<script>window.location.replace("';
+		$pageURL = 'http://';
+		if ($_SERVER["SERVER_PORT"] != "80") 
+		{
+		  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
+		} 
+		else 
+		{
+		  $pageURL .= $_SERVER["SERVER_NAME"];
+		}
+		 echo $pageURL;
+		 echo '/admin/contacts.php");</script>';
+	}
+}
+
+
+?>
