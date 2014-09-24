5e18c725277299acde1b672fb1965b60694a0f6f
Logan Voegtly <loganvoegtly@me.com>
2014-09-24 12:16:57 -0400

    Remove field narrow
    seeing if removing the field-narrow will help

--- admin/contacts.php
+++ admin/contacts.php
@@ -109,14 +109,14 @@ if ($login->isUserLoggedIn() == true)
 											<input type='text' class='stealth' name='contact_id' value='".$contact_id."'/>
 											<td>".$freezer_name."</td>
 											<td><input type='text' class='stealth' name='freezer_id[]' value='".$freezer_id."'/>".$freezer_id."</td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm0' ".$alarm0." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm1' ".$alarm1." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm2' ".$alarm2." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm3' ".$alarm3." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm4' ".$alarm4." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm5' ".$alarm5." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm6' ".$alarm6." value='1'/></td>
-											<td class='field-narrow'><input type='checkbox' class='input-medium' name='".$freezer_id."alarm7' ".$alarm7." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm0' ".$alarm0." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm1' ".$alarm1." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm2' ".$alarm2." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm3' ".$alarm3." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm4' ".$alarm4." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm5' ".$alarm5." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm6' ".$alarm6." value='1'/></td>
+											<td><input type='checkbox' class='input-medium' name='".$freezer_id."alarm7' ".$alarm7." value='1'/></td>
 										</tr>
 										";
 								
