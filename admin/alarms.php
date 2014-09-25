b4c48feb0c7c2aa4a91a43dda2ed1940ad74248a
Logan Voegtly <loganvoegtly@me.com>
2014-09-25 09:31:27 -0400

    more troubleshooting
    trying to display table correctly…

--- admin/alarms.php
+++ admin/alarms.php
@@ -125,14 +125,13 @@ if ($login->isUserLoggedIn() == true)
 					<form action='handlers/alarm-silence.php' method='POST'>
 						<input type='text' class='stealth' name='freezer_id' value='".$freezer_id."'/>
 						<input type='text' class='stealth' name='alarm_level' value='".$alarm_level."'/>
-						<td class='".$row_color." round-last><input type='submit' name='silence' class='btn-danger' value='silence'/></td>
+						<td><input type='submit' name='silence' class='btn-danger' value='silence'/></td>
 					</form>";
 					}
 					else
 					{
 						echo"
-					<td class='".$row_color." round-last>Cannot Silence</td>
-					";
+					<td class='".$row_color." ></td>";
 					}
 					echo"
 					<td><input type='text' class='input-medium search-query' name='freezer_setpoint1' value='".$freezer_setpoint1."'/></td>
