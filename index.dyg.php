369ceb3dea1d56272220715ce52cba2d273e39cf
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-26 17:25:55 -0400

    Line Breaks?

--- index.dyg.php
+++ index.dyg.php
@@ -109,7 +109,7 @@ while ($pingrow = $pings->fetch_assoc()) {
 }}
 echo "        ],
               {
-                title: '".$group." Freezers  | Location: ".$loc." | ".$hours." Hour View | 1/".$skip." Density',
+                title: '".$group." Freezers  \n Location: ".$loc." \n ".$hours." Hour View \n 1/".$skip." Density',
                 labels: [\"".$columnheader."\"],
                 colors: ['#".$colorlist."'],
                 strokeWidth: 4,
