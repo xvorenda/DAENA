921f0c35346d99a586fe2bb11d38da0261df5a3e
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-26 15:12:19 -0400

    Human Dates, Take Two

--- index.dyg.php
+++ index.dyg.php
@@ -103,6 +103,11 @@ while ($pingrow = $pings->fetch_assoc()) {
 echo "        ],
               {
                 labels: [\"".$columnheader."\"]
+              },
+              {
+                xValueFormatter: Dygraph.dateString_,
+                xValueParser: function(x) { return 1000*parseInt(x); },
+                xTicker: Dygraph.dateTicker
               });
 </script>
 ";
