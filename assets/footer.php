d10028b71abcad72e070ff9d25aeeaccfd37fc9e
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-27 12:28:32 -0400

    Kill Broken Load Time Counter

--- assets/footer.php
+++ assets/footer.php
@@ -1,24 +1,15 @@
     <div class='navbar-fixed-bottom hidden-sm hidden-xs'>
-        <p class='navbar-fixed-bottom-p'>Written for 
-        <a href='http://www.vcu.edu/' target='_blank'>VCU</a> by 
-        <a href='http://www.people.vcu.edu/~xvorenda/' target='_blank'>XVO</a> and 
-	<a href='http://www.people.vcu.edu/~voegtlylj/' target='_blank'>LJV</a> using 
-        <a href='http://www.python.org/' target='_blank'>Python</a>, 
-        <a href='http://www.mysql.com/' target='_blank'>MySQL</a>, 
-        <a href='http://www.php-login.net/' target='_blank'>PHP-Login</a>, 
-        <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>, and 
+        <p class='navbar-fixed-bottom-p'>Written for
+        <a href='http://www.vcu.edu/' target='_blank'>VCU</a> by
+        <a href='http://www.people.vcu.edu/~xvorenda/' target='_blank'>XVO</a> and
+	      <a href='http://www.people.vcu.edu/~voegtlylj/' target='_blank'>LJV</a> using
+        <a href='http://www.python.org/' target='_blank'>Python</a>,
+        <a href='http://www.mysql.com/' target='_blank'>MySQL</a>,
+        <a href='http://www.php-login.net/' target='_blank'>PHP-Login</a>,
+        <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>, and
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
-        DAENA is free software released under the GNU/GPLv3 License. <a href='http://shop.highsoft.com/faq/non-commercial' target='_blank'>HighCharts JS</a> is <b>not</b> free for commercial use.</p>
-        <div class='pageloadleft' id="loadingtime"></div><div class='pageloadright'>MySQL load time: <?PHP 
-            $endtime = microtime();
-            $mysqltime = $endtime - $starttime;
-            echo $mysqltime;?> seconds.</div>
-    </div>  
-<script type="text/javascript">
-    window.onload = function () 
-    { 
-        pageload();
-    }
-</script>
+        DAENA is free software released under the GNU/GPLv3 License.
+        <a href='http://shop.highsoft.com/faq/non-commercial' target='_blank'>HighCharts JS</a> is <b>not</b> free for commercial use.</p>
+    </div>
   </body>
 </html>
--- assets/header.php
+++ assets/header.php
@@ -2,19 +2,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script type="text/javascript">
-before = (new Date()).getTime();
-</script>
-<script type="text/javascript">
-function pageload()
-{
-    var after = (new Date()).getTime();
-    var sec = (after-before)/1000;
-    var div = document.getElementById("loadingtime");
-    div.innerHTML = "Javascript page load time: " + sec + " seconds.";
-
-}
-</script>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script src='js/bootstrap.min.js'></script>
 <title>DAENA | Data Aggregation and Emergency Notifications for Appliances</title>
