83a5b80e322a747d151892c352e4e9b676a0c254
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-27 13:15:06 -0400

    Add DyGraphs to Software List

--- assets/footer.php
+++ assets/footer.php
@@ -7,7 +7,7 @@
         <a href='http://www.mysql.com/' target='_blank'>MySQL</a>,
         <a href='http://www.php-login.net/' target='_blank'>PHP-Login</a>,
         <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>,
-        <a href='http://www.dygraphs.com/' target='_blank'>DyGraphs</a>,and
+        <a href='http://www.dygraphs.com/' target='_blank'>DyGraphs</a>, and
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
         DAENA is free software released under the GNU/GPLv3 License.
         <a href='http://shop.highsoft.com/faq/non-commercial' target='_blank'>HighCharts JS</a> is <b>not</b> free for commercial use.</p>
