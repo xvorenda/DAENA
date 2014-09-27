151cfc928d8303cdf8401bedc56863cc9975243e
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-27 00:10:42 -0400

    Maybe?

--- index.dyg.php
+++ index.dyg.php
@@ -63,5 +63,85 @@ $freezercount = count($columnnames) - 1;
 echo "
 <div id='container'></div>
 <div id='labels'></div>";
-print_r($columnnames);
+
+foreach ($columnnames as $boxname) {
+
+  echo "<input type='checkbox' id='".$boxname[1]."' onClick='change(this)' checked>
+          <label for='".$boxname[1]"'>a</label>";
+};
+echo "
+</p>
+<script type='text/javascript'>
+  g = new Dygraph(
+
+    // containing div
+    document.getElementById(\"container\"),
+        [\n";
+
+
+/* Ask MySQL for some number of minutes worth of ping data */
+$pingquery = "SELECT DISTINCT int_time FROM daena_db.data
+              WHERE int_time > ".$viewstart."
+              ORDER BY int_time ASC";
+
+$pings = $daenaDB->query($pingquery);
+
+
+$badneg_a = "-00";
+$badneg_b = "-0";
+$re_neg = "-";
+$freezergroups = implode(',', $freezerids);
+$visiblelist = implode(',', $visibility);
+
+while ($pingrow = $pings->fetch_assoc()) {
+      $pingtime = $pingrow["int_time"];
+      $pingepoch = $pingtime/1000;
+      $dataquery = "
+          SELECT temp,freezer_id
+          FROM daena_db.data
+          WHERE int_time = ".$pingtime."
+          AND freezer_id IN (".$freezergroups.")
+          ORDER BY freezer_id";
+
+      $data = $daenaDB->query($dataquery);
+
+      $datacount = $data->num_rows;
+
+      if ($datacount == $freezercount){
+        echo "            [ new Date(\"";
+        echo date('Y/m/d H:i:s', $pingepoch);
+        echo "\")";
+      while ($datarow = $data->fetch_assoc()) {
+          $datatemp = $datarow["temp"];
+          $datatemp = str_replace($badneg_a, $re_neg, $datatemp);
+          $datatemp = str_replace($badneg_b, $re_neg, $datatemp);
+          $datatemp = ltrim($datatemp, '+00');
+          $datatemp = ltrim($datatemp, '+0');
+          if ($datatemp == "nodata"){
+          $datatemp = "null";}
+          echo ", ".$datatemp;
+
+
+      }
+      echo "],\n";
+}}
+echo "        ],
+              {
+                title: '".$group." Freezers  | Location: ".$loc." | ".$hours." Hour View | 1/".$skip." Density',
+                labels: [\"".$columnheader."\"],
+                labelsDiv: document.getElementById('labels'),
+                legend: 'always',
+                colors: ['#".$colorlist."'],
+                visibility: [".$visiblelist."],
+                strokeWidth: 4,
+                drawXGrid: false,
+                axisLineColor: 'white'
+              });
+              function change(el) {
+                chart.setVisibility(el.id, el.checked);
+              }
+</script>";
+
+/* Wrap things up */
+include 'assets/footer.php';
 ?>
