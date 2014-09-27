09eb3a93965b394d73d84fbe747dda40ebcbe815
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-27 12:24:10 -0400

    Massive Cleanup
    Experiments in Misc
    Icons -> Images

--- assets/header.php
+++ assets/header.php
@@ -12,7 +12,7 @@ function pageload()
     var sec = (after-before)/1000;
     var div = document.getElementById("loadingtime");
     div.innerHTML = "Javascript page load time: " + sec + " seconds.";
-        
+
 }
 </script>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
@@ -22,7 +22,5 @@ function pageload()
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='css/bootstrap.css' rel='stylesheet'>
-<link href='css/bootstrap.daena.css' rel='stylesheet'>
+<link href='css/daena.css' rel='stylesheet'>
 <link rel='shortcut icon' href='images/daena.png'/>
-
-
