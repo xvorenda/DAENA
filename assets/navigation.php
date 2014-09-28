c346a0b8c5fd21753d35165753be2a5b4c7c732a
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-28 01:09:44 -0400

    Glyph?

--- assets/navigation.php
+++ assets/navigation.php
@@ -56,7 +56,7 @@ echo "
                    <li><a href='".$baseurl."?hours=".$hours."&group=".$group."&loc=".$loc."&type=4'>4 Fridge</a></li>
                  </ul>
                </li>
-               <li><a href='#' onclick='unzoomGraph()'>Reset</a></li>
+               <li><a class='glyphicon glyphicon-refresh' href='#' onclick='unzoomGraph()'>Reset</a></li>
              </ul>
              <ul class='nav navbar-nav navbar-right'>
                <li><a href='admin'>Admin</a></li>
