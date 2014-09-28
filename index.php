6876176292f4588969ee1e3b728e400d542d0605
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-28 19:04:12 -0400

    Different Approach

--- index.php
+++ index.php
@@ -62,9 +62,9 @@ while ($freezerrow = $freezers->fetch_assoc()) {
     array_push($visibility,"true");
     echo "<div class='freezer-box box-active'>
             <label class='click-label' for=\"".$i."\">
-              <p><span style='color: #".$colorname."'>".$freezername."</span>
+              <p class='box-spacer'><span style='color: #".$colorname."'>".$freezername."</span>
               <br>".$freezerlocation."</p>
-            </label><br>
+            </label>
             <input class='line-toggle' type=checkbox id=".$i." onClick=\"change(this)\" checked>
           </div>
             ";
