e4bc03379f1c5e739de02f10fb925d0f9192b0bc
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-28 19:15:13 -0400

    Hacky Way

--- index.php
+++ index.php
@@ -62,7 +62,8 @@ while ($freezerrow = $freezers->fetch_assoc()) {
     array_push($visibility,"true");
     echo "<div class='freezer-box box-active'>
             <label class='click-label' for=\"".$i."\">
-              <p class='box-spacer'><span style='color: #".$colorname."'>".$freezername."</span>
+              <p>
+              <span class='box-spacer'>&nbsp;</span><span style='color: #".$colorname."'>".$freezername."</span>
               <br>".$freezerlocation."</p>
             </label>
             <input class='line-toggle' type=checkbox id=".$i." onClick=\"change(this)\" checked>
