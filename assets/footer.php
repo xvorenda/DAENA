676d8ba16b59a5471d8e7a91a2ab20dd307ef308
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-27 21:59:41 -0400

    Convert to DyGraphs on Live Page

--- assets/footer.php
+++ assets/footer.php
@@ -6,11 +6,9 @@
         <a href='http://www.python.org/' target='_blank'>Python</a>,
         <a href='http://www.mysql.com/' target='_blank'>MySQL</a>,
         <a href='http://www.php-login.net/' target='_blank'>PHP-Login</a>,
-        <a href='http://www.highcharts.com/' target='_blank'>HighCharts</a>,
         <a href='http://www.dygraphs.com/' target='_blank'>DyGraphs</a>, and
         <a href='http://getbootstrap.com/' target='_blank'>Bootstrap</a> for use with telnet-capable devices and probes.<br>
-        DAENA is free software released under the GNU/GPLv3 License.
-        <a href='http://shop.highsoft.com/faq/non-commercial' target='_blank'>HighCharts JS</a> is <b>not</b> free for commercial use.</p>
+        DAENA is free software released under a GNU/GPLv3 License.</p>
     </div>
   </body>
 </html>
