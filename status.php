7de345264ed417f2fcdd1f4dd704c4afad88a3a3
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-29 19:12:04 -0400

    Enbolden

--- status.php
+++ status.php
@@ -152,7 +152,7 @@ while ($freezerrow = $freezers->fetch_assoc()) {
 
           echo "<tr class='alarm-table-row'>
               <form action='handlers/alarm-mod.php' method='POST'>
-                <td style='color:#".$colorname."'>".$freezer_name."</td>
+                <td style='font-weight:bold;color:#".$colorname."'>".$freezer_name."</td>
                 <td>".$freezer_loc."</td>
                 <td>".$freezer_setpoint1."</td>
                 <td>".$freezer_setpoint2."</td>
