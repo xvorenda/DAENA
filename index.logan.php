4b2fb11bdf6a2fd80bb39231e119d9aa055a6b20
Logan Voegtly <loganvoegtly@me.com>
2014-09-29 14:55:07 -0400

    no style
    no logan.css

--- index.logan.php
+++ index.logan.php
@@ -6,7 +6,7 @@
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='css/bootstrap.css' rel='stylesheet'>
-<link href='css/daena.logan.css' rel='stylesheet'>
+
 <link rel='shortcut icon' href='images/daena.png'/>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script type='text/javascript' src='js/dygraph-combined.js'></script>
