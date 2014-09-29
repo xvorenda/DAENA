b05a24b09271fbf241a3f2cbe163e42353b0625f
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-29 19:08:22 -0400

    Give Up And Use Open Sans

--- admin/assets/admin-header.php
+++ admin/assets/admin-header.php
@@ -17,13 +17,8 @@ echo "
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='../css/bootstrap.css' rel='stylesheet'>
 <link href='../css/daena.css' rel='stylesheet'>
-
-
-    <!--[if lt IE 9]>
-      <script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>
-      <script src='https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script>
-    <![endif]-->
-
+<link rel='shortcut icon' href='images/daena.png'/>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script type='text/javascript' src='../js/bootstrap.min.js'></script>
 <script type='text/javascript' src='../js/jscolor/jscolor.js'></script>
--- assets/header.php
+++ assets/header.php
@@ -8,6 +8,7 @@
 <link href='css/bootstrap.css' rel='stylesheet'>
 <link href='css/daena.css' rel='stylesheet'>
 <link rel='shortcut icon' href='images/daena.png'/>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
 <script type='text/javascript' src='js/dygraph-combined.js'></script>
 <script type='text/javascript' src='js/bootstrap.min.js'></script>
