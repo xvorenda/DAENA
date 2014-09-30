f7cb424e1a7c2ddd117803aac7cd1a4ac5fda00b
Logan Voegtly <loganvoegtly@me.com>
2014-09-30 09:44:00 -0400

    raw bootstrap css
    do not use the daena.css

--- index.logan.php
+++ index.logan.php
@@ -1,10 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DAENA | Data Aggregation and Emergency Notifications for Appliances</title>
+<meta charset='utf-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<link href='css/bootstrap.css' rel='stylesheet'>
+
+<link rel='shortcut icon' href='images/daena.png'/>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
+<script type='text/javascript' src='js/dygraph-combined.js'></script>
+<script type='text/javascript' src='js/bootstrap.min.js'></script>
+<script type='text/javascript' src='js/highcharts.js'></script>
+<script type='text/javascript' src='js/modules/exporting.js'></script>
+
+
 <?php
 /* Teach PHP how to read URL parameters and connect to the database, plus add defaults */
 include 'admin/config/db.php';
 include 'assets/urlvars.php';
 
 /* Get things started */
-include 'assets/header.php';
+//include 'assets/header.php';
 
 /* Define Navbar */
 include 'assets/navigation.php';
@@ -57,7 +75,7 @@ echo "
   <td>Trend</td>
   <td>Conn</td>
   <td>State</td>
-  <td>Hush</td>
+  <td>Silence</td>
 </tr>";
 
 array_push($columnnames,"Time");
