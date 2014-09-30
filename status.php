bf27d3e7664a32157693f96691ee0ae5be769d18
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-29 22:04:32 -0400

    ...?

--- status.php
+++ status.php
@@ -281,7 +281,7 @@ echo "        ],
               });
               function change(el) {
                 chart.setVisibility(el.id, el.checked);
-                $(el.parentElement).toggleClass('alarm-row-active')
+                $(el.parents().eq(2)).toggleClass('alarm-row-active')
               }
               function resetGraph()
               {
