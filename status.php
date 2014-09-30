d14a9a2a2a8996a94f7bd9214d6f3d65ed0eb4a0
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-30 16:17:17 -0400

    Actually Possible?

--- status.php
+++ status.php
@@ -166,7 +166,7 @@ while ($freezerrow = $freezers->fetch_assoc()) {
           $last_read_then = ltrim($last_read_then, '+00');
           $last_read_then = ltrim($last_read_then, '+0');
 
-          echo "<tr class='alarm-table-row alarm-row-active'  onLoad='setAlarm(".$i.")'>
+          echo "<tr class='alarm-table-row alarm-row-active'  onLoad='setAlarm(".$i.",".$current_alarm.",".$alarm_row_text.")'>
                 <td class='bold custom-font' style='color:#".$colorname."'>
                     <form action='handlers/alarm-mod.php' method='POST'>
                       <label class='status-click-label' for=\"".$i."\">
@@ -309,10 +309,10 @@ echo "        ],
               }
 </script>
 <script type='text/javascript'>
-function setAlarm(el)
+function setAlarm(el,alarm,color)
   {
-    $(el).addClass('".$current_alarm."');
-    $(el).addClass('".$alarm_row_text."');
+    $(#el).addClass('alarm');
+    $(#el).addClass('color');
   }
 </script>
 ";
