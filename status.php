d8419e2d1a9eee1c019108eed4713b68d37b2e69
X. Valentine Orenda <xvorenda@vcu.edu>
2014-09-30 01:42:02 -0400

    Finish Auto-Deselect of Test

--- status.php
+++ status.php
@@ -153,8 +153,12 @@ while ($freezerrow = $freezers->fetch_assoc()) {
           $last_temp_then = ltrim($last_temp_then, '+00');
           $last_temp_then = ltrim($last_temp_then, '+0');
 
-          echo "<tr class='alarm-table-row alarm-row-active'>
-                <td class='bold custom-font' style='color:#".$colorname."'>
+    	  if (strpos($freezerlocation,'Test') !== false) {
+            echo "<tr class='alarm-table-row'>";
+          }else{
+            echo "<tr class='alarm-table-row alarm-row-active'>";
+          };
+              echo "<td class='bold custom-font' style='color:#".$colorname."'>
                     <form action='handlers/alarm-mod.php' method='POST'>
                       <label class='status-click-label' for=\"".$i."\">
                       ".$freezer_name."
