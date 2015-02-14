f7f24cec433ce87ff9a879ae65bff012e73635d9
X. Valentine Orenda <xvorenda@gmail.com>
2015-02-14 18:17:28 -0500

    A little polish and accuracy

--- admin/assets/home.php
+++ admin/assets/home.php
@@ -1,1 +1,6 @@
-    <h1 class='custom-font'>DAENA Administration Console</h1>
+  <div id='home'>
+      <h1 class='custom-font'>DAENA Administration Console</h1>
+          <p>This space should eventually be a quick-action panel. But for now, I'll replicate the text from the About page so it's not blank when you land here.</p>
+          <p>DAENA (Data Aggregation and Emergency Notifications for Appliances) was created out of a combination of necessity and frugality - we neeeded a freezer monitoring solution, but didn't want to pay a lot of money for it. We found <a href='http://www.networkedrobotics.com/'>Networked Robotics</a> and their <a href='http://www.networkedrobotics.com/checkoutNTMS4.htm'>NTMS</a> devices, and those were cheap and effective, but the software provided only runs on Windows XP. We wanted a solution that would work in a hybrid environment, and ideally on or from any kind of device. So DAENA was born. DAENA's server components run on Python, MySQL, PHP, and Apache, and the graphs can be viewed from any browser that uses Javascript (except some versions of IE and Firefox, apparently). </p>
+          <p>Github repo at <a href='https://github.com/xvorenda/daena' target='_blank'>https://github.com/xvorenda/daena</a></p>
+  </div>
