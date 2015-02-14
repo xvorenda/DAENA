a81560b8fdc4a530b357ae4ed71d8e7980c627c3
X. Valentine Orenda <xvorenda@gmail.com>
2015-02-14 17:42:32 -0500

    Is Duplication Necessary?

--- admin/assets/admin-header.php
+++ admin/assets/admin-header.php
@@ -11,17 +11,17 @@ echo "
 <html>
 <head>
 <title>DAENA | Data Aggregation and Emergency Notifications for Appliances</title>
-<link rel='shortcut icon' href='https://daena.csbc.vcu.edu/admin/images/daena.png'/>
+<link rel='shortcut icon' href='https://daena.csbc.vcu.edu/images/daena.png'/>
 <meta charset='utf-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
-<link href='https://daena.csbc.vcu.edu/admin/css/bootstrap.css' rel='stylesheet'>
-<link href='https://daena.csbc.vcu.edu/admin/css/daena.css' rel='stylesheet'>
-<link rel='shortcut icon' href='https://daena.csbc.vcu.edu/admin/images/daena.png'/>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
+<script type='text/javascript' src='https://daena.csbc.vcu.edu/js/bootstrap.min.js'></script>
+<script type='text/javascript' src='https://daena.csbc.vcu.edu/js/jscolor/jscolor.js'></script>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-<script type='text/javascript' src='https://daena.csbc.vcu.edu/admin/js/bootstrap.min.js'></script>
-<script type='text/javascript' src='https://daena.csbc.vcu.edu/admin/js/jscolor/jscolor.js'></script>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
+<link href='https://daena.csbc.vcu.edu/css/bootstrap.css' rel='stylesheet'>
+<link href='https://daena.csbc.vcu.edu/css/daena.css' rel='stylesheet'>
+<link rel='shortcut icon' href='https://daena.csbc.vcu.edu/images/daena.png'/>
 </head>
 ";
 ?>
