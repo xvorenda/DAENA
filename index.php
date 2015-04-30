e44b4d8b0b2e39d70c96c4ac9156834c8689ff4c
X. Valentine Orenda <xvorenda@gmail.com>
2015-04-29 23:38:10 -0400

    Either fixes it or breaks it worse...

--- index.php
+++ index.php
@@ -267,8 +267,8 @@ while ($pingrow = $pingtimes->fetch_assoc()) {
             $datatemp = ltrim($datatemp, '+00');
             $datatemp = ltrim($datatemp, '+0');
             if ($datatemp == "nodata"){
-              $datatemp = "null";};
-      else $datatemp = "null";
+              $datatemp = "null";}
+        } else { $datatemp = "null";}
       echo ", ".$datatemp;
 
       }
