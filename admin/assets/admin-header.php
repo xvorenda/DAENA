445494d57f02bdfb51e0632d3daffe7f2fc1ae83
X. Valentine Orenda <xvorenda@gmail.com>
2015-05-12 16:42:59 -0400

    Reimplement Colorization

--- admin/assets/admin-header.php
+++ admin/assets/admin-header.php
@@ -16,6 +16,7 @@ echo "
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
+<script type='text/javascript' src='js/jscolor/jscolor.js'></script>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
 <link href='css/bootstrap.css' rel='stylesheet'>
 <link href='css/daena.css' rel='stylesheet'>
